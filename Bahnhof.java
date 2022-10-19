95f07b4f1c09387783be639e600cc8a41e5ab7c3
Bernhard_Rubow <brubow@newvisionsproject.de>
2022-10-19 22:58:20 +0200

    comments added

--- Bahnhof.java
+++ Bahnhof.java
@@ -1,5 +1,6 @@
 public class Bahnhof {
 
+    // Wir deklarieren die generischen Stacks
     private Stack<Waggon> gleisA;
     private Stack<Waggon> gleisB;
     private Stack<Waggon> gleisC;
@@ -7,32 +8,47 @@ public class Bahnhof {
 
     public Bahnhof() {
 
+        // wir instanzieren die einzelnen Gleise mit einem generischen Stack
         gleisA = new Stack<Waggon>();
         gleisB = new Stack<Waggon>();
         gleisC = new Stack<Waggon>();
 
-        for (int i = 0; i < 4; i++) {
-
+        // create x Waggons
+        for (int i = 0; i < 8; i++) {
             Waggon w = new Waggon();
             gleisA.push(w);
             System.out.println(w.getNummer());
         }
-
     }
 
     public void sortiereZumKleinsten() {
 
+        // Solange die Gleise A und B noch Waggons enthalten
         while (!gleisA.isEmpty() || !gleisB.isEmpty()) {
+
+            // Reset Waggon Nummer
             WaggonNummer = Integer.MAX_VALUE;
+
             while (!gleisA.isEmpty()) {
+
+                // Wir merken uns die Nummer des Waggons der zuerst
+                // aus Gleis A heraus gefahren wird
                 int topANummer = gleisA.top().getNummer();
+
+                // Ist diese Nummer kleiner als die gemerkte WaggonNummer
                 if (topANummer < WaggonNummer) {
+                    // aktualisieren wir die gemerkte Waggonnummer
                     WaggonNummer = topANummer;
                 }
+
+                // umrangieren auf Gleis B
                 gleisB.push(gleisA.top());
                 gleisA.pop();
             }
 
+            // Alle Waggons zurück zurück auf Gleis A
+            // Außer dem Waggon mit der gemerkten Waggonnummer
+            // der kommt auf Gleis C
             while (!gleisB.isEmpty()) {
                 int topBNummer = gleisB.top().getNummer();
                 if (topBNummer == WaggonNummer) {
